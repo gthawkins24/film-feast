d3a51905ce43c99a94708a573fa4ce336baa281e
Sean Collins <seancollins117@gmail.com>
2021-12-13 09:43:46 -0800

    javascript starter, doesn't currently work

---
+++ assets/js/script.js
@@ -0,0 +1,51 @@
+// select input data through id
+var genreInputEl = document.querySelector("#genre");
+var decadeInputEl = document.querySelector("#decade");
+var runtimeInputEl = document.querySelector("#runtime");
+var actorFormEl = document.querySelector("#actor-form");
+var actorInputEl = document.querySelector("#actor-name");
+var goButtonEl = document.querySelector("#go-button");
+
+
+// for submission
+var formSubmitHandler = function(event) {
+    // prevent page from reloading
+    event.preventDefault();
+
+    // get values from input elements
+    var genre = genreInputEl;
+    var decade = decadeInputEl;
+    var runtime = runtimeInputEl;
+    var actorName = actorInputEl.value.trim();
+
+    console.log(genre);
+    console.log(decade);
+    console.log(runtime);
+    console.log(actorName);
+
+    // if at least one of the variables are valid pass them all to movie recomendation function
+    if (genre || decade || runtime || actorName) {
+        getMovieRecs(genre, decade, runtime, actorName);
+
+        // clear old content
+        genreInputEl = "";
+        decadeInputEl = "";
+        runtimeInputEl = "";
+        actorInputEl.textContent = "";
+    } else {
+        alert("Please select at least 1 filter")
+    }
+};
+
+var getMovieRecs =function(genre, decade, runtime, actorName) {
+    console.log(genre);
+    console.log(decade);
+    console.log(runtime);
+    console.log(actorName);
+    console.log("function call works");
+};
+
+goButtonEl.addEventListener("click", formSubmitHandler);
+
+
+
